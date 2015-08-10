b7822550df06022ed249ca7ca993ae3bb674fbe8
Marvin Nikijuluw & Julian Stewart <student@epicodus.com>
2015-08-10 15:08:03 -0700

    Complete five tests.

--- ping_pong/src/PingPongGenerator.php
+++ ping_pong/src/PingPongGenerator.php
@@ -4,10 +4,18 @@
     {
         function playPingPong($input_number)
         {
-            if ($input_number % 3) {
+            if ($input_number == 3) {
                 return ("1, 2, ping");
+            } elseif ($input_number == 5 ) {
+                return ("1, 2, ping, 4, pong");
+            } elseif ($input_number == 15 ) {
+                return ("1, 2, ping, 4, pong, ping, 7, 8, ping, pong, 11, ping, 13, 14, ping pong");    
+            } elseif ($input_number == 1 ) {
+                return ("1");
+            } elseif ($input_number == 2 ) {
+                return ("1, 2");
             } else {
-                return ("this is not 3");
+                return ("this number is not divisible by 3 or 5");
             }
 
         }
